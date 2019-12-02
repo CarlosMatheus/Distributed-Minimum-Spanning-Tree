0dfce793d24f5c5dfab1028e7638543e52bb7958
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-12-02 06:04:06 -0300

    Adjust node color

--- run/gen_graph.py
+++ run/gen_graph.py
@@ -1,3 +1,4 @@
+from graphviz import Digraph
 
 SleepingState = "Sleeping"
 FindState = "Find"
@@ -7,15 +8,29 @@ RejectedState = "Rejected"
 BranchState = "Branch"
 BasicState = "Basic"
 
+state_to_color = {
+    SleepingState: "aquamarine",
+    FindState: "deepskyblue1",
+    FoundState: "gold1"
+}
+
 
 class Graph:
     def __init__(self):
         self.node_list = list()
         self.node_hash = dict()
+        self.edge_list = list()
+        self.edge_hash = dict()
 
-    def add_node(self, id, state, ):
-        self.node_list.append(Node(id, state))
-        self.node_hash[id] = 1
+    def add_edge(self, edge):
+        self.edge_list.append(edge)
+        self.edge_hash[edge.weight] = edge
+
+    def add_node(self, id, state):
+        node = Node(id, state, self)
+        self.node_list.append(node)
+        self.node_hash[id] = node
+        return node
 
     def has_node_id(self, node_id):
         return node_id in self.node_hash
@@ -23,15 +38,29 @@ class Graph:
     def get_node_by_id(self, node_id):
         return self.node_hash[node_id]
 
+    def print_graph(self):
+        f = Digraph('graph', filename='graph.gv')
+
+        for node in self.node_list:
+            # f.attr(style='filled', color=state_to_color[node.state])
+            f.node(node.id, style='filled', color=state_to_color[node.state])
+
+        for edge in self.edge_list:
+            f.edge(edge.from_node.id, edge.to_node.id, label=edge.weight)
+
+        f.view()
+
 
 class Node:
-    def __init__(self, id, state):
+    def __init__(self, id, state, graph):
         self.id = id
         self.state = state
         self.neighbors_list = list()
+        self.graph = graph
 
     def add_edge(self, edge):
         self.neighbors_list.append(edge)
+        self.graph.add_edge(edge)
 
 
 class Edge:
@@ -58,12 +87,12 @@ def make_graph():
                 print(node_id_2)
 
                 if not graph.has_node_id(node_id_1):
-                    node_1 = Node(node_id_1, SleepingState)
+                    node_1 = graph.add_node(node_id_1, SleepingState)
                 else:
                     node_1 = graph.get_node_by_id(node_id_1)
 
                 if not graph.has_node_id(node_id_2):
-                    node_2 = Node(node_id_2, SleepingState)
+                    node_2 = graph.add_node(node_id_2, SleepingState)
                 else:
                     node_2 = graph.get_node_by_id(node_id_2)
 
@@ -77,7 +106,10 @@ def make_graph():
                 node_1.add_edge(edge1)
                 node_2.add_edge(edge2)
 
+    return graph
 
-make_graph()
+
+graph = make_graph()
+graph.print_graph()
 
 
