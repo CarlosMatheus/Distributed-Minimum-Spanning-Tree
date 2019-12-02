c1e2a3205fa3f566bd8f6bdda5b9866fd17f4e70
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-12-02 05:25:24 -0300

    Make graph read work

--- run/gen_graph.py
+++ run/gen_graph.py
@@ -3,6 +3,7 @@ SleepingState = "Sleeping"
 FindState = "Find"
 FoundState = "Found"
 
+
 class Graph:
     def __init__(self):
         self.node_list = list()
@@ -15,6 +16,7 @@ class Graph:
     def has_node(self, node_id):
         return node_id in self.node_hash
 
+
 class Node:
     def __init__(self, id, state):
         self.id = id
@@ -23,6 +25,7 @@ class Node:
 
     def add_edge(self):
         pass
+
 
 class Edge:
     def __init__(self, from_node, to_node, weight, state):
@@ -34,13 +37,19 @@ class Edge:
 
 def make_graph():
     file = open('graph.txt', 'r')
-    row_list = file.open().split('\n')
+    row_list = file.read().split('\n')
     for row in row_list:
         if row:
-            if not row.startswith('graph') or not row.startswith('}'):
+            if not row.startswith('graph') and not row.startswith('}'):
                 begin, end = row.split(' -- ')
                 node_id_1 = begin[-1]
                 node_id_2 = end[0]
                 print(node_id_1)
                 print(node_id_2)
+
+
+
+
+make_graph()
+
 
